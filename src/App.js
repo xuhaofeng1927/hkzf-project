d4d4c360a12185c5566b0f1c5ae8b6e69b48a5b3
xuhaofeng <xuhaofeng@163.com>
2020-03-09 22:30:17 +0800

    全局导航-配置路由-首页重定向

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import CityList from './pages/CityList'
 import Map from './pages/Map'
 import NotFound from './pages/Fn404'
 // import { Button } from 'antd-mobile';
-import { BrowserRouter as Router, Route,Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route,Switch,Redirect} from 'react-router-dom'
 function App() {
   return (
     <Router className="app">
       {/* 一级路由导航 */}
         <Switch>
-          <Route path="/" exact component={Home} />
+          <Route path="/" exact render={()=>{
+            return <Redirect to="/home" />
+          }} />
           <Route path="/home" component={Home} />
           <Route path="/cityList" component={CityList} />
           <Route path="/map" component={Map} />
