6ee1135acf1f46540bc73406e054dedb09ceacab
xuhaofeng <xuhaofeng@163.com>
2020-03-11 20:04:13 +0800

    全局导航-底部Tabbar-重构

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,80 +11,58 @@ import '../../assets/fonts/iconfont.css'
 import Index from './Default'
 import House from './House'
 import Profile from './Profile'
+// 引入导航栏数据
+import tabItems from '../../untils/tabTtems' 
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedTab: 'redTab',
+      selectedTab: props.location.pathname,
       hidden: false,
       fullScreen: false,
     };
   }
-  
+  // 导航栏
+  renderTabBarItem=()=> {
+    return  tabItems.map((Item)=>{
+      return (
+      <TabBar.Item
+       title={Item.title}
+       key="1"
+       icon={<i className={Item.icon} />}
+       selectedIcon={<i className={Item.icon} />}
+       selected={this.props.location.pathname === Item.path}
+       onPress={() => {
+         this.props.history.push(Item.path)
+         this.setState({
+           selectedTab: Item.path,
+         });
+       }}
+       data-seed="logId"
+     >
+     </TabBar.Item>
+     )
+    })
+  }
   render() {
     return (
       <div>
         {/* <Link to="/home/index">首页</Link>
       <Link to="/home/house">找房</Link>
      <Link to="/home/profile">我的</Link> */}
-
+        <Route exact path='/home/' component={Index} />
+        <Route exact path='/home/house' component={House} />
+        <Route exact path='/home/profile' component={Profile} />
         <div className="barBox">
+          {/* 导航栏 */}
           <TabBar
             unselectedTintColor="#949494"
             tintColor="#33A3F4"
             barTintColor="white"
             hidden={this.state.hidden}
           >
-            <TabBar.Item
-              title="首页"
-              key="1"
-              icon={<i className="iconfont icon-ind" />}
-              selectedIcon={<i className="iconfont icon-ind" />}
-              selected={this.props.location.pathname === '/home'}
-              onPress={() => {
-                this.props.history.push('/home')
-                this.setState({
-                  selectedTab: '/home',
-                });
-              }}
-              data-seed="logId"
-            >
-              <Route exact path="/home" component={Index} />
-            </TabBar.Item>
-            <TabBar.Item
-              icon={<i className="iconfont icon-findHouse"/>}
-              selectedIcon={<i className="iconfont icon-findHouse"/>}
-              title="找房"
-              key="2"        
-              selected={this.props.location.pathname === '/home/house'}
-              onPress={() => {
-                this.props.history.push('/home/house')
-                this.setState({
-                  selectedTab: '/home/house'
-                });
-              }}
-              data-seed="logId1"
-            >
-             <Route path="/home/house" component={House} />
-            </TabBar.Item>
-            <TabBar.Item
-              icon={<i className="iconfont icon-my" />}
-              selectedIcon={<i className="iconfont icon-my"/>}
-              title="我的"
-              key="3"
-              dot
-              selected={this.props.location.pathname === '/home/profile'}
-              onPress={() => {
-                this.props.history.push('/home/profile')
-                this.setState({
-                  selectedTab: '/home/profile',
-                });
-              }}
-            >
-              <Route path="/home/profile" component={Profile} />
-            </TabBar.Item>
-
+           {this.renderTabBarItem()}
           </TabBar>
         </div >
       </div>
---
+++ src/untils/tabTtems.js
@@ -0,0 +1,17 @@
+export default [
+    {
+      title: '首页',
+      path: '/home',
+      icon: 'iconfont icon-ind',
+    },
+    {
+      title: '找房',
+      path: '/home/house',
+      icon: 'iconfont icon-findHouse',
+    },
+    {
+      title: '我的',
+      path: '/home/profile',
+      icon: 'iconfont icon-my',
+    },
+  ]
