2647deaa54c90b81f89e7fe4f9b6a320dfac98f4
xuhaofeng <xuhaofeng@163.com>
2020-03-12 18:39:12 +0800

    首页-轮播图-请求数据并渲染

--- src/pages/Home/Default/index.js
+++ src/pages/Home/Default/index.js
@@ -1,40 +1,48 @@
 import React from 'react';
 import { Carousel } from 'antd-mobile';
+import axios from 'axios';
 
 class Default extends React.Component {
   state = {
-    data: ['1', '2', '3'],
+    // 轮播图数据
+    CarouselList: [],
+    // 轮播图占位
     imgHeight: 176,
   }
+  // 获取轮播图数据
+  getCarousel= async() => {
+    const {data} = await axios.get('http://localhost:8080/home/swiper')
+    this.setState({
+      CarouselList: data.body
+    });
+    
+  }
   componentDidMount() {
-    // simulate img loading
-    setTimeout(() => {
-      this.setState({
-        data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
-      });
-    }, 100);
+    // 获取轮播图数据
+    this.getCarousel()
   }
   render() {
     return (
       <div className="home">
         {/* 轮播图 */}
           <Carousel
+            // 自动播放控制
             autoplay={false}
+            //循环播放
             infinite
-
           >
-            {this.state.data.map(val => (
+            {this.state.CarouselList.map(Item => (
               <a
-                key={val}
+                key={Item.id}
                 href="http://www.alipay.com"
                 style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
               >
                 <img
-                  src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                  src={`http://localhost:8080${Item.imgSrc}`}
                   alt=""
                   style={{ width: '100%', verticalAlign: 'top' }}
                   onLoad={() => {
-                    // fire window resize event to change height
+                    //根据窗口大小事件改变高度
                     window.dispatchEvent(new Event('resize'));
                     this.setState({ imgHeight: 'auto' });
                   }}
