d56bb41ef0a4b662a958665fbd64fbe646650c46
xuhaofeng <xuhaofeng@163.com>
2020-03-13 20:56:35 +0800

    首页-栏目导航-布局-调整样式

--- src/pages/Home/Default/index.js
+++ src/pages/Home/Default/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Carousel } from 'antd-mobile';
+import { Carousel, Flex } from 'antd-mobile';
 // 引入基础地址
-import {BASE_URL} from '../../../untils/request'
-// 映入首页需要的接口
-import {getSwiper} from '../../../api/home' 
+import { BASE_URL } from '../../../untils/request'
+// 引入首页需要的接口
+import { getSwiper } from '../../../api/home'
+// 引入首页需要的数据
+import { navs } from '../../../untils/homeItems'
+// 引入样式
+import './index.css'
 class Default extends React.Component {
   state = {
     // 轮播图数据
@@ -11,56 +15,74 @@ class Default extends React.Component {
     // 轮播图占位
     imgHeight: 176,
     // 自动播放开关
-    autoplay:false
+    autoplay: false
   }
   // 获取轮播图数据
-  getCarousel= async() => {
-    const {status,data} = await getSwiper()  
-    if (status===200) {
+  getCarousel = async () => {
+    const { status, data } = await getSwiper()
+    if (status === 200) {
       this.setState({
         CarouselList: data
-      },()=>{
+      }, () => {
         this.setState({
-          autoplay:true
+          autoplay: true
         })
       });
-    } 
-    
-    
+    }
   }
+
   componentDidMount() {
     // 获取轮播图数据
     this.getCarousel()
   }
+  // 轮播图视图渲染
+  // carouselData = () => {
+  // }
+  // 栏目导航试图渲染
+  // navData = () => {
+  // }
   render() {
     return (
       <div className="home">
         {/* 轮播图 */}
-          <Carousel
-            // 自动播放控制
-            autoplay={this.state.autoplay}
-            //循环播放
-            infinite
-          >
-            {this.state.CarouselList.map(Item => (
-              <a
-                key={Item.id}
-                href="http://www.alipay.com"
-                style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
-              >
-                <img
-                  src={`${BASE_URL}${Item.imgSrc}`}
-                  alt=""
-                  style={{ width: '100%', verticalAlign: 'top' }}
-                  onLoad={() => {
-                    //根据窗口大小事件改变高度
-                    window.dispatchEvent(new Event('resize'));
-                    this.setState({ imgHeight: 'auto' });
-                  }}
-                />
-              </a>
-            ))}
-          </Carousel>
+        <Carousel
+          // 自动播放控制
+          autoplay={this.state.autoplay}
+          //循环播放
+          infinite
+        >
+          {this.state.CarouselList.map(Item => (
+            <a
+              key={Item.id}
+              href="http://www.alipay.com"
+              style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+            >
+              <img
+                src={`${BASE_URL}${Item.imgSrc}`}
+                alt=""
+                style={{ width: '100%', verticalAlign: 'top' }}
+                onLoad={() => {
+                  //根据窗口大小事件改变高度
+                  window.dispatchEvent(new Event('resize'));
+                  this.setState({ imgHeight: 'auto' });
+                }}
+              />
+            </a>
+          ))}
+        </Carousel>
+
+        {/* 栏目导航 */}
+        <Flex className="nav">
+          {navs.map(Item => {
+            return (
+              <Flex.Item onClick={() => console.log(Item.id)
+              } key={Item.id}>
+                <img src={Item.img} alt={Item.title} />
+                <p>{Item.title}</p>
+              </Flex.Item>
+            )
+          })}
+        </Flex>
       </div>
     )
   }
